1deb653c008636e3e5171700fac44b7c6889fb95
Andrew Rowley <andrew.rowley@leonardo.com>
2025-07-22 14:52:41 +0100

    fix tests

--- test_main.py
+++ test_main.py
@@ -1,3 +1,6 @@
-
+from main import rand_generator
 def test_rand_generator():
+    for _ in range(100):
+        result = rand_generator(1, 10)
+        assert 1 <= result <= 10, f"Value {result} not in range"
     return
